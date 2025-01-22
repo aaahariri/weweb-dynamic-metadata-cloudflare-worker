7f9b0e3b402b56f2aa5bfcaa7f91dd52c297df97
aaahariri <ala7lam@ala7lam.com>
2025-01-23 00:49:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://0b9d0bbe-4407-47de-9831-6e2902a99c26.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
     {
       pattern: "/p/([^/]+)/([^/]+)", // Matches /p/{var1}/{var2}
